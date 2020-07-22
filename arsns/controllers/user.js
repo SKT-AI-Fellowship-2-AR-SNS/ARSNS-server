b23c45a4824c08c27089256cb875df8d4c2574a4
HyeonJooo <parkhj2416@gmail.com>
2020-07-22 16:44:22 +0900

    controllers

---
+++ arsns/controllers/user.js
@@ -0,0 +1,10 @@
+const util = require('../modules/util');
+const resMessage = require('../modules/responseMessage');
+const statusCode = require('../modules/statusCode');
+// const UserModel = require('../models/user');
+
+module.exports = {
+    postFace : async(req, res) =>{
+        
+    }
+}
