a43954a78c5eb86dd3d2ed3513dc5f8abe732e5e
HyeonJooo <parkhj2416@gmail.com>
2020-09-04 16:01:52 +0900

    이미지 multer수정

--- arsns/routes/main/index.js
+++ arsns/routes/main/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const mainController = require('../../controllers/main');
-// const upload = require('../../modules/multer');
-const multer = require('multer');
-const upload = multer({
-    dest: 'upload/'
-});
+const upload = require('../../modules/multer');
+// const multer = require('multer');
+// const upload = multer({
+//     dest: 'upload/'
+// });
 
 router.post('/getLocation', mainController.getLocation);
-router.post('/addHistory', upload.single('image'), mainController.addHistory);
+router.post('/addHistory', upload.array('image',1), mainController.addHistory);
 router.get('/getHistory/:myid/:yourid/:bssid1/:bssid2', mainController.getHistory);
 router.get('/getFriendHistory/:myId/:friendId', mainController.getFriendHistory);
 router.get('/getPersonName/:id', mainController.getPersonName);
