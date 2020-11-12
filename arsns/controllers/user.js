fe8c17ee12999163bf7e68c1e1064e9be3723201
HyeonJooo <parkhj2416@gmail.com>
2020-11-12 15:20:29 +0900

    editText 수정

--- arsns/controllers/user.js
+++ arsns/controllers/user.js
@@ -253,7 +253,8 @@ module.exports = {
             res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
             return;
         }
-
+        console.log("id: ", id);
+        console.log("message: ", message);
         const result = await User.editText(id, message);
         return res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.EDIT_PROFILE_SUCCESS, result));
     },
